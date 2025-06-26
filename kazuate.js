354fd45b47ad8366fe9a88b7a82fdae9370e754e
G484922024 <248492@st.takushoku-u.ac.jp>
2025-06-26 17:45:08 +0900

    数当てゲーム

--- kazuate.js
+++ kazuate.js
@@ -8,39 +8,40 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('button');
+b.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  // テキストボックスに指定された数値を yoso に代入する
+  let i = document.querySelector('input#t');
+  let yoso = Number(i.value);
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
   kaisu = kaisu + 1;
-  console.log(kaisu+'回目の予想: '+yoso);
+  let p = document.querySelector('span#yoso');
+  let p2 = document.querySelector('span#kaisu');
+  p2.textContent = kaisu + '回目の予想: ' + yoso;
   if(kaisu >= 4){
-    console.log('答えは'+kotae+'でした.すでにゲームは終わっています');
+    p.textContent = '答えは'+kotae+'でした.すでにゲームは終わっています';
   }
   else if(yoso === kotae){
-    console.log('正解です.おめでとう!');
+    p.textContent = '正解です.おめでとう!';
     kaisu = 4;
   }
   else if(yoso !== kotae){
     if(kaisu === 3){
-        console.log('まちがい.残念でした答えは'+kotae+'です');
+        p.textContent = 'まちがい.残念でした答えは'+kotae+'です';
     }
     else if(kaisu <= 2 && yoso < kotae){
-        console.log('まちがい.答えはもっと大きいですよ');
+        p.textContent = 'まちがい.答えはもっと大きいですよ';
     }
     else if(kaisu <= 2 && yoso > kotae){
-        console.log('まちがい.答えはもっと小さいですよ');
+        p.textContent = 'まちがい.答えはもっと小さいですよ';
     }
   }
 
-
 }
+
